05ddaaeb92f80ae170d5c42707e30c611a39a860
Josue Brenes <josuebrenes3005@gmail.com>
2023-02-02 15:57:37 -0600

    First Commit

--- Ejercicio 9.py
+++ Ejercicio 9.py
@@ -1,15 +1,21 @@
+# = Le solicitamos al usuario sus ingresos mensuales
 print("Digite sus ingresos mensuales")
 ingresos = input()
 ingresos = int(ingresos)
 
+# = Le solicitamos cuanto gasta mensualmente en alimentacion
 print("Digite sus gastos mensuales en alimentacion")
 gastos = input()
 gastos = int(gastos)
 
+# = Ahora sacamos el porcentaje correspondiente al rubro de alimentacion 
 total = ((gastos * 100) // ingresos)
+
+# = Imprimimos el resultado correspondiente
 print("El gasto correspondiente al rubro de alimentacion es de: " + str(total) + "%")
 
+# = Calculamos cuanto le queda para otros rubros que es el 100% - total
 restante = (100 - total)
+
+# = Imprimimos el resultado correspondiente
 print("El porcentaje disponible para otros rubros es de " + str(restante) + "%")
-  
-#Prueba
